ed91fb1e33c745cddc1ac32696319ccee7554435
LOGESH M <logesh.m0324@gmail.com>
2026-01-08 11:00:52 +0530

    Admin Repository Commit

--- app/domains/admin/repository.py
+++ app/domains/admin/repository.py
@@ -1,8 +1,38 @@
-from core.database import db
+from core.database import get_db
 
 class AdminRepository:
+
     async def create_tenant(self, tenant: dict):
-        await db.tenants.insert_one(tenant)
+        db = await get_db()
+        result = await  db.tenants.insert_one(tenant)
+
+        tenant["_id"] = str(result.inserted_id)  # ✅ convert ObjectId
+        print("Tenant created in repository:", tenant)
+        return tenant
 
     async def list_tenants(self):
-        return await db.tenants.find().to_list(100)
+        db = await get_db()
+        result = await db.tenants.find().to_list(100)
+        for tenant in result:
+            tenant["_id"] = str(tenant["_id"])  # ✅ convert ObjectId
+        return result
+
+    async def get_tenant(self, tenant_id: str):
+        db = await get_db()
+        tenant = await db.tenants.find_one({"tenant_id": tenant_id})
+
+        if tenant:
+            tenant["_id"] = str(tenant["_id"])  # ✅ convert ObjectId
+        return tenant
+
+    async def update_tenant(self, tenant_id: str, data: dict):
+        db = await get_db()
+        await db.tenants.update_one(
+            {"tenant_id": tenant_id},
+            {"$set": data}
+        )
+        return await self.get_tenant(tenant_id)
+
+    async def delete_tenant(self, tenant_id: str):
+        db = await get_db()
+        await db.tenants.delete_one({"tenant_id": tenant_id})
