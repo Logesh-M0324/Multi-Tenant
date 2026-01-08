00e61eb399e71b92ed853832a3ceee124179e059
LOGESH M <logesh.m0324@gmail.com>
2026-01-08 11:03:09 +0530

    Admin Service commit

--- app/domains/admin/service.py
+++ app/domains/admin/service.py
@@ -2,14 +2,27 @@ from domains.admin.repository import AdminRepository
 from shared.utils import generate_id
 
 class AdminService:
+
     def __init__(self):
         self.repo = AdminRepository()
 
-    async def create_tenant(self, name: str):
+    async def create_tenant(self, name: str, status: str):
         tenant = {
-            "_id": generate_id(),
+            "tenant_id": generate_id(),
             "name": name,
-            "status": "ACTIVE"
+            "status": status
         }
-        await self.repo.create_tenant(tenant)
-        return tenant
+        print("Creating tenant in service:", tenant)
+        return await self.repo.create_tenant(tenant)
+
+    async def list_tenants(self):
+        return await self.repo.list_tenants()
+
+    async def get_tenant(self, tenant_id: str):
+        return await self.repo.get_tenant(tenant_id)
+
+    async def update_tenant(self, tenant_id: str, status: str):
+        return await self.repo.update_tenant(tenant_id, {"status": status})
+
+    async def delete_tenant(self, tenant_id: str):
+        await self.repo.delete_tenant(tenant_id)
