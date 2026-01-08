1153e2123f686c599ece5bf115029ff05c758667
LOGESH M <logesh.m0324@gmail.com>
2026-01-08 11:04:53 +0530

    Auth router commit

--- app/domains/auth/router.py
+++ app/domains/auth/router.py
@@ -1,10 +1,16 @@
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends, HTTPException, Security
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 from domains.auth.schemas import SignUpRequest, LoginRequest, TokenResponse
 from domains.auth.service import AuthService
 from core.database import get_db
-
+from core.dependencies import get_current_user,auth_scheme #oauth2_scheme,
+from core.token_blacklist import TokenBlacklist
+from jose import jwt
+from core.config import settings
 
 router = APIRouter(prefix="/auth", tags=["Auth"])
+
+#auth_scheme = HTTPBearer()
 
 # ----------------------------
 # Sign Up
@@ -34,6 +40,7 @@ async def sign_up(payload: SignUpRequest, db = Depends(get_db)):
 async def login(payload: LoginRequest, db = Depends(get_db)):
     try:
         service = AuthService(database=db)
+        print("service in login", service)
         return await service.login(payload.email, payload.password)
     except Exception as e:
         raise HTTPException(status_code=401, detail=str(e))
@@ -41,12 +48,15 @@ async def login(payload: LoginRequest, db = Depends(get_db)):
 # ----------------------------
 # Logout
 # ----------------------------
-@router.post("/logout")
-async def logout():
-    """
-    In a real enterprise zero-trust system, token revocation would require:
-    - Redis blacklist
-    - Short-lived JWTs
-    For MVP, we just return OK
-    """
-    return {"status": "logged out"}
+ 
+@router.post("/logout") 
+async def logout( token: HTTPAuthorizationCredentials = Depends(auth_scheme), user = Depends(get_current_user) ):
+    try: 
+        print("logging out") # Extract the raw JWT string 
+        decoded = jwt.decode( token.credentials, settings.JWT_SECRET, algorithms=[settings.JWT_ALGO] ) 
+        print("decoded") 
+        await TokenBlacklist.revoke(token.credentials, decoded["exp"])
+        print("revoked") 
+        return {"status": "success", "message": "Token revoked"} 
+    except jwt.JWTError: 
+        raise HTTPException(status_code=401, detail="Invalid token")
